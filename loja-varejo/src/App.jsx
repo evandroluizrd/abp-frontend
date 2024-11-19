c55b89a790cc8ff803143341b99d6458e1a67ae6
HianRick <hianrmacedo@gmail.com>
2024-11-19 20:08:38 -0300

    agora tem react router :)

--- loja-varejo/src/App.jsx
+++ loja-varejo/src/App.jsx
@@ -1,6 +1,21 @@
+import {
+    createBrowserRouter,
+    RouterProvider
+   } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+    path: '/',
+    element: <Home />,
+    },
+   ]);
+   export function App() {
+    return <RouterProvider router={router} />;
+   }
+
 function app(){
 return(
-    <div className="introducao">
+    <div className="">
         <h1> Bem vindo a loja varejo</h1>
         <p> Feito por...</p>
     </div>
