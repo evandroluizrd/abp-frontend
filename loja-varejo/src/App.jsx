349197f8f29b6266010a910fcb6b6a611cf01b8b
HianRick <hianrmacedo@gmail.com>
2024-11-19 20:10:53 -0300

    1

--- loja-varejo/src/App.jsx
+++ loja-varejo/src/App.jsx
@@ -1,23 +1,8 @@
-import {
-    createBrowserRouter,
-    RouterProvider
-   } from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-    path: '/',
-    element: <Home />,
-    },
-   ]);
-   export function App() {
-    return <RouterProvider router={router} />;
-   }
-
 function app(){
 return(
-    <div className="">
+    <div className="marge-solid">
         <h1> Bem vindo a loja varejo</h1>
-        <p> Feito por...</p>
+        <p> Feito por... min</p>
     </div>
 )
 }
